522c40fb210219b1f46ed473fea4fee1178215aa
Vandan647 <vandanbond007@gmail.com>
2025-12-25 22:17:34 +0530

    Add database connection and login logic
    Implemented database connection and login functionality.

--- Login.java
+++ Login.java
@@ -1,10 +1,16 @@
 package campusComplaints;
 
 import javax.swing.*;
+
 import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
 
 public class Login extends JFrame {
 	
+	Connection conn;
+    Statement stmt;
+    ResultSet rs;
 	 ImageIcon i1 = new ImageIcon(
 	            Login.class.getResource("loginicon.jpg")
 	        );
@@ -56,7 +62,80 @@ public class Login extends JFrame {
 	 REGISTER.setBounds(620, 400, 100, 40);
 	 add(l0);
 	 l0.setBounds(650,20,100,60);
-    
+	 LOGIN.addActionListener(new log());
+	 REGISTER.addActionListener(new reg());
+	 connectDB() ;
+		
+	}
+	
+	// Database connection method
+    private void connectDB() 
+     {
+        try 
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/BusManagement","root","Root@1234");
+            System.out.println("Database Connected.");
+        } 
+        catch (Exception e) 
+        {
+            JOptionPane.showMessageDialog(this, "Database Connection Failed: " + e.getMessage());
+        }
+    }
+
+	class log implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			 
+     	    String roll = f1.getText();
+     	    String password = String.valueOf(f2.getPassword());
+
+     	    try {
+     	        String query = "select * from Student where RollNumber = ? and password = ?;";
+     	        PreparedStatement pst = conn.prepareStatement(query);
+
+     	        pst.setString(1, roll);
+     	        pst.setString(2, password);
+     	   
+     	         rs = pst.executeQuery();
+                 
+     	         if(rs.next())
+     	         {
+     	        	
+     	        	new studentDashBoard(roll);
+     	         }
+     	        else 
+                {
+                  JOptionPane.showMessageDialog(Login.this, "Invalid Username or Password!");
+                }
+
+          }
+     	    
+          catch (SQLException ex) 
+          {
+              JOptionPane.showMessageDialog(Login.this, "Error: " + ex.getMessage());
+          }
+
+     	       
+     	  
+     	        
+		} 
+		   
+			
+		}
+		
+	
+	class reg implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		    new RegistrationForm();
+			
+		}
 		
 	}
    
@@ -65,3 +144,4 @@ public class Login extends JFrame {
 		new Login();
 	}
 }
+
